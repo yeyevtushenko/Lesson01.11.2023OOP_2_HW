712cfbddb6ed196ca631a856705bc51f8eadb62e
yeyevtushenko <yehor803@gmail.com>
2023-11-15 20:00:43 +0200

    Реалізуйте клас «Вебсайт». Збережіть у класі: назву вебсайту, адресу та опис вебсайту. Реалізуйте конструктор та методи класу для введення-виведення даних, а також для інших операцій. Використовуйте механізм перевантаження методів.

--- Task 5.py
+++ Task 5.py
@@ -3,14 +3,16 @@
 # Реалізуйте конструктор та методи класу для введення-виведення даних,
 # а також для інших операцій. Використовуйте механізм перевантаження методів.
 print("Завдання №5")
+
+
 class Website:
-    websites_list = []
+    websites = []
 
     def __init__(self, name, address, description):
         self.name = name
         self.address = address
         self.description = description
-        Website.websites_list.append(self)
+        Website.websites.append(self)
 
     def display_info(self):
         print("Назва вебсайту: ", self.name)
@@ -55,4 +57,42 @@ class Website:
     def add_website(cls, name, address, description):
         new_website = cls(name, address, description)
         print(f"Додано новий вебсайт: {new_website.name}")
+
+website1 = Website("Google", "https://www.google.com", "Пошукова система та інші сервіси")
+website2 = Website("Facebook", "https://www.facebook.com", "Соціальна мережа")
+website3 = Website("Twitter", "https://www.twitter.com", "Соціальна мережа та мікроблогінг")
+
+while True:
+    add_new_site = input("Чи хочете ввести новий вебсайт? (так/ні): ").lower()
+
+    if add_new_site != 'так':
+        break
+
+    name = input("Введіть назву вебсайту: ")
+    address = input("Введіть адресу вебсайту: ")
+    description = input("Введіть опис вебсайту: ")
+
+    Website.add_website(name, address, description)
+
+print("\nІнформація про всі вебсайти:")
+for site in Website.websites:
+    site.display_info()
+
+websites = [website1, website2, website3]
+
+for i in range(len(websites)):
+    for j in range(i + 1, len(websites)):
+        if websites[i] != websites[j]:
+            print(f"{websites[i].name} та {websites[j].name} - різні вебсайти за призначенням.")
+        else:
+            print(f"{websites[i].name} та {websites[j].name} - однакові вебсайти за призначенням.")
+
+
+for i in range(len(websites)):
+    for j in range(i + 1, len(websites)):
+        if websites[i] != websites[j]:
+            print(f"{websites[i].name} та {websites[j].name} - різні вебсайти за призначенням.")
+        else:
+            print(f"{websites[i].name} та {websites[j].name} - однакові вебсайти за призначенням.")
+
 
