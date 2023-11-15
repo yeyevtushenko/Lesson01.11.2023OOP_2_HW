3db2f78cc28518fa27e1cec7b0bab72594d22b43
yeyevtushenko <yehor803@gmail.com>
2023-11-15 19:44:03 +0200

    Реалізуємо клас Website

--- Task 5.py
+++ Task 5.py
@@ -3,3 +3,13 @@
 # Реалізуйте конструктор та методи класу для введення-виведення даних,
 # а також для інших операцій. Використовуйте механізм перевантаження методів.
 print("Завдання №5")
+class Website:
+    websites_list = []
+
+    def __init__(self, name, address, description):
+        self.name = name
+        self.address = address
+        self.description = description
+        Website.websites_list.append(self)
+
+
