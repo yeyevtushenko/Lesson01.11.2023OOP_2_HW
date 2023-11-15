f675e8f1aa927c5c7e76934c7f1881daf0507f18
yeyevtushenko <yehor803@gmail.com>
2023-11-15 19:19:21 +0200

    додано методи перевантаження

--- Task 4.py
+++ Task 4.py
@@ -11,3 +11,31 @@ class Watch:
         self.year = year
         self.price = price
         self.watch_type = watch_type
+
+    def display_info(self):
+        print("Модель годинника: ", self.model)
+        print("Виробник: ", self.manufacturer)
+        print("Рік випуску: ", self.year)
+        print("Ціна: ", self.price)
+        print("Тип годинника: ", self.watch_type)
+
+    def update_price(self, new_price):
+        self.price = new_price
+        print("Ціна годинника оновлена.")
+
+    def update_info(self, new_model, new_manufacturer, new_year, new_watch_type):
+        self.model = new_model
+        self.manufacturer = new_manufacturer
+        self.year = new_year
+        self.watch_type = new_watch_type
+        print("Інформацію про годинник оновлено.")
+
+    def __str__(self):
+        return f"{self.model}, {self.manufacturer}, {self.year}, {self.price}, {self.watch_type}"
+
+    def __eq__(self, other):
+        return (
+                isinstance(other, Watch) and
+                self.model == other.model and
+                self.manufacturer == other.manufacturer
+        )
