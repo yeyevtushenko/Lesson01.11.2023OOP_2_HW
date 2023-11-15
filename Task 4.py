3e48c1621371489aeb79fc1cfaedff0ad8b6521f
yeyevtushenko <yehor803@gmail.com>
2023-11-15 19:14:50 +0200

    Створюємо клас Watch

--- Task 4.py
+++ Task 4.py
@@ -4,3 +4,10 @@
 # Реалізуйте конструктор та методи класу для введення-виведення даних, а також для інших операцій.
 # Використовуйте механізм перевантаження методів.
 print("Завдання №4")
+class Watch:
+    def __init__(self, model, manufacturer, year, price, watch_type):
+        self.model = model
+        self.manufacturer = manufacturer
+        self.year = year
+        self.price = price
+        self.watch_type = watch_type
